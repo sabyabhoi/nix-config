bfe18deb4ad8443f15c45c15266b9c17e71394b1
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2026-03-14 22:13:41 +0530

    Add segment tree snippet

--- modules/home-modules/packages.nix
+++ modules/home-modules/packages.nix
@@ -7,6 +7,7 @@
 
   home.packages = with pkgs;
     [
+      lutris
       niri
       noctalia-shell
       ardour
--- modules/network.nix
+++ modules/network.nix
@@ -16,15 +16,15 @@
       block = ["porn"];
     };
 
-    # nameservers = [
-    #   "1.1.1.1"
-    #   "1.0.0.1"
-    #   "2606:4700:4700::1111"
-    #   "2606:4700:4700::1001"
-    # ];
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
     resolvconf.enable = true;
     firewall = {
-      enable = true;
+      enable = false;
       allowedTCPPorts = [80 443 20];
     };
   };
