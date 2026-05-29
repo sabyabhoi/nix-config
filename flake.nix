4ae82b6bb792a6683b46d56afba787154293e413
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2026-05-29 08:02:31 +0530

    Revert "Improve caching"
    This reverts commit 2559ff5133fc18d32d6d7b3f53cb764be229dc3f.

--- flake.nix
+++ flake.nix
@@ -1,19 +1,6 @@
 {
   description = "Configuration Flake";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://devenv.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHCSMMTM0UlKoUV7jOiap7VP0YsUPk7Egr3t6KY6s="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "devenv.cachix.org-1:EpJVZb1wF/uY21WYSO5Q/WE8s+5gX7I/YNG0Nc6Y3o="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -27,10 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    stylix.url = "github:danth/stylix";
   };
 
   outputs = {
