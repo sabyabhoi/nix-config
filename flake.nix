3f78f994ea2477c6b504bacb6ed51d5532d862dc
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2026-08-14 19:19:10 +0530

    Pin stylix to nixos

--- flake.nix
+++ flake.nix
@@ -14,7 +14,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix.url = "github:danth/stylix";
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
